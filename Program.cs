85b6d6908c627433e187d7bcc7fbba63e32a4a78
Marina Litvinova <mari.s.litvinova@yandex.ru>
2022-12-04 12:08:42 +0300

    Все задачи к 3-му семинару

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// int Num (){
-//     int num = Convert.ToInt32(Console.ReadLine());
-//     int hun = num /100;
-//     int dec = num /10;
-//     return (dec - hun *10);
-//     }
-// Console.WriteLine("Введите трёхзначное число:");
-// int result = Num();
-// Console.WriteLine($"Получено число - {result}");
+int Num (){
+    int num = Convert.ToInt32(Console.ReadLine());
+    int hun = num /100;
+    int dec = num /10;
+    return (dec - hun *10);
+    }
+Console.WriteLine("Введите трёхзначное число:");
+int result = Num();
+Console.WriteLine($"Получена цифра - {result}");
 
 
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
