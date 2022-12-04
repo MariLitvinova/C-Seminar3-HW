aba10ca3e6aff0f65f36e0c310c2ac6e41e89302
Marina Litvinova <mari.s.litvinova@yandex.ru>
2022-12-04 11:57:10 +0300

    Добавлено решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -10,15 +10,16 @@
 // int result = Num();
 // Console.WriteLine($"Получено число - {result}");
 
+
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
-    
+
 // Console.WriteLine("Введите число:");
 // int num = Convert.ToInt32(Console.ReadLine());
-// int count = 0;
+// int count = 0;    
 // if (num  /100 >= 1)
 // {
 //     int Test (){
-//         while( (num /1000) >= 1 )
+//         while( num /1000 >= 1 )
 //         {
 //             num = num /10;
 //             count++;
@@ -27,9 +28,27 @@
 //         return (num - help1 *10);
 //     }
 //     int result = Test();
-//     Console.WriteLine($"Получено число - {result}");
+//     Console.WriteLine($"Получена цифра - {result}");
 // }
 // else
 //     {
-//         Console.WriteLine("Третьего числа нет");
+//         Console.WriteLine("Третьей цифры нет");
 //     }
+
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// void Week(int day){
+//     if (day == 6){
+//         Console.WriteLine("да");
+//     }
+//     else if (day == 7){
+//         Console.WriteLine("да");
+//     }
+//     else{
+//         Console.WriteLine("нет");
+//     }
+// }
+// Console.WriteLine("Введите число, соответствующее дню недели:");
+// int day = Convert.ToInt32(Console.ReadLine());
+// Week(day);
